3cbc4c93ebd9398c1f0b08b4a93ab1b981a34e0c
sophiaallui <sophiaallui@gmail.com>
2021-05-16 11:18:18 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,4 @@
 // script.js
-/*
-import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
-const setState = router.setState;
-*/ 
-// Make sure you register your service worker here too
-
-/*
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://cse110lab6.herokuapp.com/entries')
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach(entry => {
-        let newPost = document.createElement('journal-entry');
-        newPost.entry = entry;
-        document.querySelector('main').appendChild(newPost);
-
-        newPost.addEventListener('click', () => {
-          let curEntry = router.findEntry(entry); 
-
-          window.history.pushState({page_id: 1, entry: entry}, "", "#entry" + curEntry);
-          setState(entry); 
-
-      });
-    });
-  });
-
-  document.querySelector('h1').addEventListener('click', ()=> { 
-
-    if(window.location.hash != ""){ 
-      window.history.pushState({page_id: 0}, "", window.location.origin + "/Lab7/"); 
-      setState(); 
-    }
-
-  }); 
-
-  document.querySelector('img').addEventListener('click', () => {
-    if(window.location.hash != "#settings"){ 
-      window.history.pushState({page_id:2}, "", "#settings"); 
-    }
-  });
-
-  // Moving through session history
-  window.addEventListener('popstate', (event) => {
-    if(event.state == null) {
-      setState();
-      return;
-    }
-    
-    if(event.state.page_id == 1) {
-      setState(event.state.entry);
-    }else {
-      setState();
-    }
-  });
-  
-}); 
-
-//Service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-*/ 
-
-// script.js
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
@@ -95,6 +22,16 @@ document.addEventListener('DOMContentLoaded', () => {
         });
       });
     });
+    
+    //Change back to main page when title is clicked 
+    document.querySelector('h1').addEventListener('click', ()=> { 
+      //Don't nav to itself 
+      if (window.location.hash != ""){ 
+        //Push home state
+        window.history.pushState({page_id: 0}, "home", window.origin + "/Lab7/");
+        setState(); 
+      }
+    });
 
     //Change to settings
     document.querySelector('img').addEventListener('click', ()=> {
@@ -106,15 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
 
-    //Change back to main page when title is clicked 
-    document.querySelector('h1').addEventListener('click', ()=> { 
-      //Don't nav to itself 
-      if (window.location.hash != ""){ 
-        //Push home state
-        window.history.pushState({page_id: 0}, "home", window.origin + "/Lab7/");
-        setState(); 
-      }
-    });
+
 
     window.addEventListener('popstate', (event)=>{
       //If event.state == null, we're back at origin
